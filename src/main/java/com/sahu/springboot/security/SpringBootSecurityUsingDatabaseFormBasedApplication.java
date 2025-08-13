b1edfe464d5de81517cbd7afa5cd1551c2b8fa25
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-08-13 23:04:55 +0530

    Security related classes

---
+++ src/main/java/com/sahu/springboot/security/SpringBootSecurityUsingDatabaseFormBasedApplication.java
@@ -0,0 +1,13 @@
+package com.sahu.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityUsingDatabaseFormBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityUsingDatabaseFormBasedApplication.class, args);
+	}
+
+}
